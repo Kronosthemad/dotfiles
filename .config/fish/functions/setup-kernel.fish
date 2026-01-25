ecd2a014b65f9c1ac04ab43253bca8e499b5573a
Eli Weatherby <chemicalorca22@gmail.com>
2026-01-25 17:49:01 -0600

    doom configs added

--- .config/fish/functions/setup-kernel.fish
+++ .config/fish/functions/setup-kernel.fish
@@ -1,9 +1,11 @@
+#!/usr/bin/env fish
+
 function setup-kernel
-	pkgctl repo clone --protocol=https linux
-	cd linux
-	git clone --depth 1 https://github.com/Kronosthemad/kronos-linux
-	cp config ./kronos-linux/.config
-	cd kronos-linux
-	make prepare
-	make menuconfig
+    pkgctl repo clone --protocol=https linux
+    cd linux
+    git clone --depth 1 https://github.com/Kronosthemad/kronos-linux
+    cp config ./kronos-linux/.config
+    cd kronos-linux
+    make prepare
+    make menuconfig
 end
